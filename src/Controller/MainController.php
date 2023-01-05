fdec28d1c62ad68d7ce0c51a7c4445a19d1b96b3
Julian Calegari <julian.calegari13330@gmail.com>
2023-01-05 18:18:08 +0100

    horaire footer dans le footer

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller;
 
+use App\Repository\OpeningTimeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
-    #[Route('/main', name: 'app_main')]
-    public function index(): Response
+    #[Route('/main', name: 'main')]
+    public function index(OpeningTimeRepository $openingTimeRepository): Response
     {
-        return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
+        //return $this->render('main/index.html.twig', [
+            return $this->render('_partials/_footer.html.twig', [
+            'dayMethode' => $openingTimeRepository->findAll()
         ]);
     }
+  
 }
