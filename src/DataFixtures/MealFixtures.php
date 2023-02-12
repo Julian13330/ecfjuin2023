83716550bac30c4d0bda5e71782342a08798cce1
Julian Calegari <julian.calegari13330@gmail.com>
2023-02-12 10:45:51 +0100

    Mise en forme des plats

--- src/DataFixtures/MealFixtures.php
+++ src/DataFixtures/MealFixtures.php
@@ -12,24 +12,6 @@ class MealFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         // Les entrées
-         $accra = new Meal();
-         $accra->setTitle('Accras de Morue');
-         $accra->setDescription('La recette familiale en provenance du Portugal');
-         $accra->setImageName('');
-         $accra->setPrice(14,00);
-         // getReference qui me permet d'ajouter la catégorie à mon plat.
-         $accra->setCategory($this->getReference(CategoryFixtures::CATEGORY_ENTREE));
-         $manager->persist($accra);
-
-         $panisse = new Meal();
-         $panisse->setTitle('Panisses marseillaises');
-         $panisse->setDescription('Recette locale, végétarienne et sans gluten !');
-         $panisse->setImageName('');
-         $panisse->setPrice(12,00);
-         // getReference qui me permet d'ajouter la catégorie à mon plat.
-         $panisse->setCategory($this->getReference(CategoryFixtures::CATEGORY_ENTREE));
-         $manager->persist($panisse);
-
          $soupe = new Meal();
          $soupe->setTitle('La soupe de poissons');
          $soupe->setDescription('Croutons,rouille,emmental et gousse d\'ail');
@@ -40,14 +22,6 @@ class MealFixtures extends Fixture
          $manager->persist($soupe);
 
          // Les plats mer
-         $homard = new Meal();
-         $homard->setTitle('Les ravioles de Homard');
-         $homard->setDescription('Pâte à raviole à l\'encre de seiches,sauce au beurre ');
-         $homard->setImageName('');
-         $homard->setPrice(36,00);
-         // getReference qui me permet d'ajouter la catégorie à mon plat.
-         $homard->setCategory($this->getReference(CategoryFixtures::CATEGORY_MER));
-         $manager->persist($homard);
 
          $dorade = new Meal();
          $dorade->setTitle('Le filet de Dorade');
@@ -68,15 +42,6 @@ class MealFixtures extends Fixture
          $entrecôte->setCategory($this->getReference(CategoryFixtures::CATEGORY_TERRE));
          $manager->persist($entrecôte);
 
-         $raviole = new Meal();
-         $raviole->setTitle('Les ravioles cèpes et foie gras');
-         $raviole->setDescription('Pâte fraiche parfumée à la truffe, bouillon forestier');
-         $raviole->setImageName('');
-         $raviole->setPrice(29,00);
-         // getReference qui me permet d'ajouter la catégorie à mon plat.
-         $raviole->setCategory($this->getReference(CategoryFixtures::CATEGORY_TERRE));
-         $manager->persist($raviole);
-
          // Les desserts
          $chocolat = new Meal();
          $chocolat->setTitle('Mousse au chocolat');
@@ -87,14 +52,6 @@ class MealFixtures extends Fixture
          $chocolat->setCategory($this->getReference(CategoryFixtures::CATEGORY_DESSERT));
          $manager->persist($chocolat);
 
-         $cafe = new Meal();
-         $cafe->setTitle('Café gourmand');
-         $cafe->setDescription('Assortiment de trois pâtisseries');
-         $cafe->setImageName('');
-         $cafe->setPrice(12,00);
-         // getReference qui me permet d'ajouter la catégorie à mon plat.
-         $cafe->setCategory($this->getReference(CategoryFixtures::CATEGORY_DESSERT));
-         $manager->persist($cafe);
 
         $manager->flush();
     }
