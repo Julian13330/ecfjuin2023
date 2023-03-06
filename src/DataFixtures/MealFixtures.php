b6bf8bae064c2ffbbe5d7ce4ce15ec5082af252a
Julian Calegari <julian.calegari13330@gmail.com>
2023-03-06 20:23:16 +0100

    datafixtures image plats

--- src/DataFixtures/MealFixtures.php
+++ src/DataFixtures/MealFixtures.php
@@ -6,6 +6,7 @@ use App\DataFixtures\CategoryFixtures;
 use App\Entity\Meal;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class MealFixtures extends Fixture
 {
@@ -15,7 +16,7 @@ class MealFixtures extends Fixture
          $soupe = new Meal();
          $soupe->setTitle('La soupe de poissons');
          $soupe->setDescription('Croutons,rouille,emmental et gousse d\'ail');
-         $soupe->setImageName('');
+         $soupe->setImageName('soupe-de-poisson-63f23c6301355069455382.jpg');
          $soupe->setPrice(14,00);
          $soupe->setFavoris('yes');
          // getReference qui me permet d'ajouter la catégorie à mon plat.
@@ -29,7 +30,7 @@ class MealFixtures extends Fixture
          $dorade->setDescription('Risotto aux champignons, sauces aux girolles et au foie gras');
          $dorade->setPrice(32,00);
          $dorade->setFavoris('yes');
-         $dorade->setImageName('');
+         $dorade->setImageName('fish-g09a112b2e-640-64036e3919e20551056557.jpg');
          // getReference qui me permet d'ajouter la catégorie à mon plat.
          $dorade->setCategory($this->getReference(CategoryFixtures::CATEGORY_MER));
          $manager->persist($dorade);
@@ -38,7 +39,7 @@ class MealFixtures extends Fixture
          $entrecôte = new Meal();
          $entrecôte->setTitle('Entrecôte grillée');
          $entrecôte->setDescription('Entrecôte race Limousine, environ 260 grammes');
-         $entrecôte->setImageName('');
+         $entrecôte->setImageName('entrecote.jpg');
          $entrecôte->setPrice(26,00);
          $entrecôte->setFavoris('yes');
          // getReference qui me permet d'ajouter la catégorie à mon plat.
@@ -49,7 +50,7 @@ class MealFixtures extends Fixture
          $chocolat = new Meal();
          $chocolat->setTitle('Mousse au chocolat');
          $chocolat->setDescription('Mousse au chocolat fait maison, glace vanille');
-         $chocolat->setImageName('');
+         $chocolat->setImageName('nougat-gb7b4b5723-640-64036e8b71185672639148.jpg');
          $chocolat->setPrice(10,00);
          $chocolat->setFavoris('yes');
          // getReference qui me permet d'ajouter la catégorie à mon plat.
