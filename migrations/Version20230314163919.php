c2e39b96dfb4760332c22ce9e8e7a4cc6b59f6ad
Julian Calegari <julian.calegari13330@gmail.com>
2023-03-14 17:40:29 +0100

    nouvel essaie de migration

--- migrations/Version20230314163919.php
+++ migrations/Version20230314163919.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230313151716 extends AbstractMigration
+final class Version20230314163919 extends AbstractMigration
 {
     public function getDescription(): string
     {
