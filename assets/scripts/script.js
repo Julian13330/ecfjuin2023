0df94f9eadd90604190faf0b164c0fd84c7a1596
Julian Calegari <julian.calegari13330@gmail.com>
2023-05-16 17:36:36 +0200

    js modif

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -18,26 +18,22 @@ window.onscroll=function(){
 
 // Système de filtre par catégorie pour les plâts
 
-// récupère-les buttons
+// QuerySelectorAll sur mes bouttons
 const filterButtons = Array.from(document.querySelectorAll('.btn-filter'));
 
-// event listener
+// Listener
 filterButtons.forEach(button => {
     button.addEventListener('click', e => {
-        // récupère la valeur de l'attribut data-filter du button
+        // Prend la valeur de data.filter
         const filter = e.target.dataset.filter;
-        // récupère-les elements à modifier par la suite
+        // Récupère par catégorie
         const items = document.querySelectorAll('.categorie-section');
 
-
-        // tout afficher si all est sélectionné
+        // Afficher tout les plats
         if (filter === 'all') {
             items.forEach(item => item.style.display = 'block');
         } else {
-            // sinon cacher tous les éléments
-            items.forEach(item => item.style.display = 'none');
-
-            // et ne montrer que celui qui est sélectionné
+            // Afficher les plats sélectionné
             document.querySelectorAll(`[data-category="${filter}"]`).forEach(item => item.style.display = 'block');
         }
     });
